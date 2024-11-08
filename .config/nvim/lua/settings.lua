fc9828787b341526ef1640b016753b25f5cc1029
neo_3477 <gaebelmarco@googlemail.com>
2024-11-08 07:52:38 +0100

    initialer upload

---
+++ .config/nvim/lua/settings.lua
@@ -0,0 +1,10 @@
+-- Allgemeine Einstellungen
+vim.opt.number = true  -- Zeilennummern anzeigen
+vim.opt.cursorline = false      -- Aktuelle Zeile hervorheben
+vim.opt.smartindent = true         -- Intelligente Einrückung
+vim.opt.hlsearch = true            -- Suche hervorheben
+vim.opt.termguicolors = true      -- Verwende 24-Bit-Farben
+vim.opt.incsearch = true           -- Inkrementelle Suche
+vim.opt.termguicolors = true
+-- Setze <leader> auf Leertaste
+vim.g.mapleader = " "
