aac905b9a70d3f19cf866d38baff50e3b614df47
neo_3477 <gaebelmarco@googlemail.com>
2024-11-08 07:53:22 +0100

    initialer upload

---
+++ .config/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,45 @@
+-- Alpha-Nvim konfigurieren
+local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
+local devicons = require("nvim-web-devicons")
+
+-- Beispielbildschirm für das Dashboard (du kannst diesen Abschnitt anpassen)
+dashboard.section.header.val = {
+    [[                                                                     ]],
+    [[       ███████████           █████      ██                     ]],
+    [[      ███████████             █████                             ]],
+    [[      ████████████████ ███████████ ███   ███████     ]],
+    [[     ████████████████ ████████████ █████ ██████████████   ]],
+    [[    █████████████████████████████ █████ █████ ████ █████   ]],
+    [[  ██████████████████████████████████ █████ █████ ████ █████  ]],
+    [[ ██████  ███ █████████████████ ████ █████ █████ ████ ██████ ]],
+    [[ ██████   ██  ███████████████   ██ █████████████████ ]],
+    [[ ██████   ██  ███████████████   ██ █████████████████ ]],
+}
+
+-- Header farblich anpassen
+vim.api.nvim_set_hl(0, "DashboardHeader", { fg = "#458588" }) -- Hex-Code für die gewünschte Farbe
+
+-- Der Header wird der neuen Highlight-Gruppe zugewiesen
+dashboard.section.header.opts.hl = "DashboardHeader"
+
+
+
+-- Menüoptionen (Beispiel)
+dashboard.section.buttons.val = {
+  dashboard.button("e", "  New File", ":ene <BAR> startinsert <CR>"),
+  dashboard.button("f", "󰮗  Find File", ":Telescope find_files<CR>"),
+  dashboard.button("r", "  Recent Files", ":Telescope oldfiles<CR>"),
+  dashboard.button("x", "  Explorer (cwd)", ":Neotree<CR>"),
+  dashboard.button('o', '󰇈  Obsidian', ':e $HOME/Documents/Neo3477 <CR>'),
+  dashboard.button('c', '  .config', ':e $HOME/.config <CR>'),
+  dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+}
+
+-- Footer Text
+dashboard.section.footer.val = "Have a productive day!"
+
+-- Setup des Dashboards
+alpha.setup(dashboard.opts)
+
+
