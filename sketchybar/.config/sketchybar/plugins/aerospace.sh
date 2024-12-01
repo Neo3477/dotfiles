d4f5c46959b56f4cb7d364d7c30e695e86672ff9
neo_3477 <gaebelmarco@googlemail.com>
2024-12-01 13:10:05 +0100

    change border color

--- sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -4,7 +4,7 @@ SPACE_ID="$1"
 FOCUSED_WORKSPACE="$AEROSPACE_FOCUSED_WORKSPACE"
 
 if [ "$SPACE_ID" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set space.$SPACE_ID icon.color=0xff83a598
+    sketchybar --set space.$SPACE_ID icon.color=0xff89a4fb
 else
-    sketchybar --set space.$SPACE_ID icon.color=0xfffb4934
+    sketchybar --set space.$SPACE_ID icon.color=0xfff38ba8
 fi
