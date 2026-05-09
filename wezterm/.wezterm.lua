2c1cdcc7d911eac6393dae28cf983c26c9a1c67a
neo_3477 <gaebelmarco@googlemail.com>
2026-05-09 23:43:58 +0200

    change wezterm start size

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -11,8 +11,8 @@ config.font = wezterm.font("JetBrainsMono Nerd Font Mono", {weight = "Thin"})
 config.font_size = 15
 
 --höhe und breite des Fensters
-config.initial_cols = 179
-config.initial_rows = 43
+config.initial_cols = 174
+config.initial_rows = 40
 
 config.adjust_window_size_when_changing_font_size = false
 
