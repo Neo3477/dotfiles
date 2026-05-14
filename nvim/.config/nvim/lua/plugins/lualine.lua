1c6d77181fa41af605e57a70f6b17cee0ac18489
neo_3477 <gaebelmarco@googlemail.com>
2026-05-14 10:43:00 +0200

    change theme settings

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -33,7 +33,7 @@ local bubbles_theme = {
 
 require('lualine').setup {
   options = {
-    theme = "catppuccin",
+    theme = "catppuccin-mocha",
     component_separators = '',
     section_separators = { left = '', right = '' },
   },
